3b8c1db915fc5bf3a8498438a2ea81cb9e9b3a96
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-07-05 00:13:29 +0530

    added home carousal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/pages/homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <div>
-        home
+        <HomePage/>
       </div>
     </div>
   );
---
+++ src/customer/pages/homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousal from '../../components/homecarousal/MainCarousal'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousal/>
+        <div>
+        other section
+    </div>
+    </div>
+    
+  )
+}
+
+export default HomePage;
