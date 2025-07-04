3a6570e5f12bb727445a3cf6ecfd63b3245c0210
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-07-04 21:01:00 +0530

    Added nav bar from tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <Navigation/>
+      <div>
+        home
+      </div>
     </div>
   );
 }
--- src/customer/components/navigation/Navigation.jsx
+++ src/customer/components/navigation/Navigation.jsx
@@ -141,7 +141,7 @@ const navigation = {
   ],
 }
 
-export default function Example() {
+export default function Navigation() {
   const [open, setOpen] = useState(false)
 
   return (
