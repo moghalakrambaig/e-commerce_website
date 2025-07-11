3e983b30f0fb438a2a9d58337ceaf716cd2742e0
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-07-11 13:20:18 +0530

    logo added on home page

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -291,7 +291,7 @@ export default function Navigation() {
                   <span className="sr-only">Your Company</span>
                   <img
                     alt=""
-                    //src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    src={require("../../images/web_logo.jpeg")}
                     className="h-8 w-auto"
                   />
                 </a>
