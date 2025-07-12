32173a67804383ec1dad92d76fc6bb374cae4bdc
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-07-13 01:53:10 +0530

    created cloudinary account and also uploded images to cloudinary and access them using url/added prefix URL

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,7 +4,7 @@ import ProductCard from './ProductCard';
 const Product = () => {
     return (
         <div>
-            <ProductCard />
+            <ProductCard cardImage={"https://res.cloudinary.com/dh6g9tp6p/image/fetch/w_256,h_256,c_fill,f_auto,q_auto/https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ea22c193-f576-4353-975d-d7eda69bde53/FLEX+RUNNER+3+%28PS%29.png"} />
         </div>
 
     )
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({cardImage} ) => {
     return (
         <div className='productCard mt-5 ml-10 flex flex-col items-center bg-white w-full max-w-[16rem]'>
             <div className='w-full h-[16rem] overflow-hidden rounded-lg'>
-                <img className='object-cover object-top w-full h-full' src="https://res.cloudinary.com/demo/image/fetch/w_256,h_256,c_fill,f_auto,q_auto/https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ea22c193-f576-4353-975d-d7eda69bde53/FLEX+RUNNER+3+%28PS%29.png" alt=''></img>
+                <img className='object-cover object-top w-full h-full' src={cardImage} alt=''></img>
             </div>
-            <div className='textPart bg-white p-3 text-center'>
+            <div className='textPart bg-white p-1 text-center'>
                 <p className='font-bold opacity-60'>Nike Flex Runner 3</p>
                 <p>Younger Kids' Shoes</p>
+            </div>
+            <div className='flex items-center space-x-2'>
+                <p className='font-semibold'>$200</p>
+                <p className='line-through opacity-45'>$300</p>
+                <p className='text-green-500 font-semibold'>70% Off</p>
             </div>
         </div>
 
