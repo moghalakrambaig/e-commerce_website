df652eb5a2d5b0c94d54d8a20f4acd36b20cb02c
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-07-13 05:31:00 +0530

    added product list category filters componenet from tailwind css

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -272,7 +272,7 @@ export default function Navigation() {
           Get free delivery on orders over $100
         </p> */}
 
-        <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <nav aria-label="Top" className="w-full px-4 sm:px-6 lg:px-9">
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
               <button
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import './ProductCard.css';
 
-const ProductCard = ({cardImage} ) => {
+const ProductCard = ({ cardImage }) => {
     return (
-        <div className='productCard mt-5 ml-10 flex flex-col items-center bg-white w-full max-w-[16rem]'>
-            <div className='w-full h-[16rem] overflow-hidden rounded-lg'>
-                <img className='object-cover object-top w-full h-full' src={cardImage} alt=''></img>
+        <div className='productCard mt-5 mx-auto flex flex-col items-center rounded-lg shadow-md overflow-hidden bg-white transition-all w-full max-w-[19rem]'>
+            <div className=' w-full'>
+                <img className='object-cover object-top w-full h-auto' src={cardImage} alt='Product' />
             </div>
-            <div className='textPart bg-white p-1 text-center'>
+            <div className='textPart text-center bg-white p-2'>
                 <p className='font-bold opacity-60'>Nike Flex Runner 3</p>
                 <p>Younger Kids' Shoes</p>
-            </div>
-            <div className='flex items-center space-x-2'>
-                <p className='font-semibold'>$200</p>
-                <p className='line-through opacity-45'>$300</p>
-                <p className='text-green-500 font-semibold'>70% Off</p>
+                <div className='flex space-x-2 justify-center mt-1'>
+                    <p className='font-semibold'>$200</p>
+                    <p className='line-through opacity-45'>$300</p>
+                    <p className='text-green-500 font-semibold'>70% Off</p>
+                </div>
             </div>
         </div>
-
-    )
-}
+    );
+};
 
 export default ProductCard;
